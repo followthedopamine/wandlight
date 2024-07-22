11c2b6d45e61c6546440110a9ae41d9c39bfe56a
followthedopamine <83159854+followthedopamine@users.noreply.github.com>
2024-07-22 12:34:43 +1200

    Refactor: Cleaned up naming conventions for exploration/fog script

--- scenes/exploration_scene.gd
+++ scenes/exploration_scene.gd
@@ -1,34 +1,35 @@
 extends Node2D
 
-const LightTexture = preload("res://sprites/placeholder/Light.png")
+const LIGHT_TEXTURE = preload("res://sprites/placeholder/Light.png")
 const GRID_SIZE = 16
 
 @onready var fog = $Fog
+@onready var player = $PlayerCharacterBody2D
 
-var display_width = 1280#ProjectSettings.get("display/window/size/viewport_width")
-var display_height = 720#ProjectSettings.get("display/window/size/viewport_height")
+var display_width = ProjectSettings.get("display/window/size/viewport_width")
+var display_height = ProjectSettings.get("display/window/size/viewport_height")
 
-var fogImage = Image.new()
-var fogTexture = ImageTexture.new()
-var lightImage = LightTexture.get_image()
-var light_offset = Vector2(LightTexture.get_width()/2, LightTexture.get_height()/2)
+var fog_image = Image.new()
+var fog_texture = ImageTexture.new()
+var light_image = LIGHT_TEXTURE.get_image()
+var light_offset = Vector2(LIGHT_TEXTURE.get_width()/2, LIGHT_TEXTURE.get_height()/2)
 
 func _ready():
 	var fog_image_width = display_width/GRID_SIZE
 	var fog_image_height = display_height/GRID_SIZE
-	fogImage = Image.create(fog_image_width, fog_image_height, false, Image.FORMAT_RGBAH)
-	fogImage.fill(Color.BLACK)
-	lightImage.convert(Image.FORMAT_RGBAH)
+	fog_image = Image.create(fog_image_width, fog_image_height, false, Image.FORMAT_RGBAH)
+	fog_image.fill(Color.BLACK)
+	light_image.convert(Image.FORMAT_RGBAH)
 	fog.scale *= GRID_SIZE
 
 func update_fog(new_grid_position):
-	var light_rect = Rect2(Vector2.ZERO, Vector2(lightImage.get_width(), lightImage.get_height()))
-	fogImage.blend_rect(lightImage, light_rect, new_grid_position - light_offset)
+	var light_rect = Rect2(Vector2.ZERO, Vector2(light_image.get_width(), light_image.get_height()))
+	fog_image.blend_rect(light_image, light_rect, new_grid_position - light_offset)
 	update_fog_image_texture()
 
 func update_fog_image_texture():
-	fogTexture = ImageTexture.create_from_image(fogImage)
-	fog.texture = fogTexture
+	fog_texture = ImageTexture.create_from_image(fog_image)
+	fog.texture = fog_texture
 
-func _input(event):
-	update_fog(get_local_mouse_position()/GRID_SIZE)
+func _physics_process(delta):
+	update_fog(player.position/GRID_SIZE)
